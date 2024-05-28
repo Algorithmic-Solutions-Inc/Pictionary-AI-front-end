61f7ba3a51d199efcd06c6b6e141a6f05a38ba68
Armen Agabekian <2008nv@gmail.com>
2024-05-28 15:48:39 -0700

    works-timer

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 'use strict';
 require('dotenv').config()
 const io = require('socket.io-client');
@@ -8,27 +7,55 @@ const optionMap = {}; // Map options to letters
 const socket = io(clientURL); // Assuming the server is running locally on port 3000
 let isCorrect = null;
 let CORRECT_ANSWER = null;
+let QUESTION = null;
+
+const readline = require('readline');
+const {shuffle} = require("./utility");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
 socket.on('connect', () => {
     console.log('Connected to server');
     socket.emit('fetchTriviaQuestion'); // Request a trivia question when connected
 });
 
-socket.on('triviaQuestion', ({question, options, correctAnswer}) => {
+function clearConsole() {
+    // Clear the console
+    const blank = '\n'.repeat(process.stdout.rows);
+    console.log(blank);
+    rl.write(null, { ctrl: true, name: 'l' });
+}
+
+socket.on('timer', (timeLeft) => {
+    clearConsole();
+    console.log(`Time remaining: ${timeLeft} seconds`);
+    console.log('Trivia Question:', QUESTION);
+    // Print options
+    for (const [key, value] of Object.entries(optionMap)) {
+        console.log(`${key}. ${value}`);
+    }
+    askForGuess(CORRECT_ANSWER, optionMap);
+});
+
+socket.on('triviaQuestion', ({ question, options, correctAnswer }) => {
     CORRECT_ANSWER = correctAnswer;
+    QUESTION = question;
+
     console.log('Trivia Question:', question);
-    console.log('Options :');
     options.forEach((option, index) => {
         console.log(`${String.fromCharCode(65 + index)}. ${option}`);
     });
-
-    options.forEach((option, index) => {
+let shuffled = shuffle(options);
+    console.log("SHUFFLED:", options);
+    shuffled.forEach((option, index) => {
         optionMap[String.fromCharCode(65 + index)] = option;
     });
 
-    askForGuess(correctAnswer, optionMap); // Ask for guess after displaying the question
+    socket.emit('timer'); // Start the timer when the question is received
+    socket.off('TriviaQuestion', question);
 });
-
 
 socket.on('error', (message) => console.error('Error:', message));
 
@@ -38,7 +65,6 @@ function askForGuess(correctAnswer, optionMap) {
     process.stdin.resume(); // Resume input stream
     process.stdin.once('data', (data) => {
         const guess = data.toString().trim().toUpperCase(); // Convert the guess to uppercase
-        // console.log("XXXX", guess, optionMap[guess])
 
         if (!optionMap[guess]) {
             console.error('Invalid input. Please enter a valid option.');
@@ -46,21 +72,23 @@ function askForGuess(correctAnswer, optionMap) {
             return;
         }
         isCorrect = optionMap[guess] === correctAnswer; // Compare the guess with the correct answer
-        // console.log("isCorrect", isCorrect)
         socket.emit('sendGuess', guess, isCorrect); // Send the guess and whether it's correct to the server
         process.stdin.pause(); // Pause input stream
+
     });
 }
 
 socket.on('guessAcknowledgment', (acknowledgment) => {
-        if (isCorrect) {
-            console.log('Your guess is correct!\n');
-        } else {
-            console.log('Incorrect guess. Better luck next time!');
-            // askForGuess(CORRECT_ANSWER, optionMap); // option:another guess if incorrect
-        }
+    if (isCorrect) {
+        console.log('Your guess is correct!\n');
+    } else {
+        console.log('Incorrect guess. Better luck next time!');
+
     }
-);
+    socket.off('guessAcknowledgment', acknowledgment);
+});
+
+
 // Handle unexpected errors
 process.on('uncaughtException', (error) => {
     console.error('An unexpected error occurred:', error);
